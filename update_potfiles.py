e44bbacba03f84ac48893a50a5d87945f8c7c489
Heliguy <heliguy4599@hotmail.com>
2025-04-23 00:11:05 -0400

    Update update_potfiles.py

--- update_potfiles.py
+++ update_potfiles.py
@@ -38,6 +38,7 @@ def write_to_file(files):
     with open(POTFILES_PATH, "w") as f:
         for line in files:
             f.write(f"{line}\n")
+            print("Wrote", line, "to", POTFILES_PATH)
 
 
 if __name__ == "__main__":
@@ -51,3 +52,4 @@ if __name__ == "__main__":
 
     translation_files = find_translation_files()
     write_to_file(translation_files)
+    print("Updated", POTFILES_PATH, "\n")
