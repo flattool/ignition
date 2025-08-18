21dd023baac3cdfbc7809fb7f8d008aee2a46013
Heliguy <heliguy4599@hotmail.com>
2025-08-17 23:05:39 -0400

    Update TS specific options and types

--- types/ambient.d.ts
+++ types/ambient.d.ts
@@ -1,7 +1,4 @@
 declare function _(id: string): string
-declare function print(...args: any[]): void
-declare function log(obj: object, others?: object[]): void
-declare function log(msg: string, substitutions?: any[]): void
 
 declare const pkg: {
 	app_id: string
@@ -16,29 +13,9 @@ declare const pkg: {
 	version: string
 }
 
-declare namespace console {
-	export function error(obj: object, others?: object[]): void
-	export function error(msg: string, substitutions?: any[]): void
-}
-
 declare interface String {
 	format(...replacements: string[]): string
 	format(...replacements: number[]): string
-}
-declare interface Number {
-	toFixed(digits: number): number
-}
-
-declare const TextDecoder: {
-	new (
-		encoding?: string,
-		options?: { fatal?: boolean; ignoreBOM?: boolean },
-	): {
-		decode(
-			input?: ArrayBuffer | ArrayBufferView,
-			options?: { stream?: boolean },
-		): string
-	}
 }
 
 type Callable<Args extends any[], Rtrn extends any, This extends any = void> = (this: This, ...args: Args)=> Rtrn
