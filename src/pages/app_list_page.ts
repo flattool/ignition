d0e29456a912933f71c68a8f4b63d779a7baa335
Heliguy <heliguy4599@hotmail.com>
2025-09-11 00:26:14 -0400

    Add searching and initial filtering to AppList

--- src/pages/app_list_page.ts
+++ src/pages/app_list_page.ts
@@ -1,16 +1,13 @@
 import Adw from "gi://Adw?version=1"
 import Gtk from "gi://Gtk?version=4.0"
-import Gio from "gi://Gio?version=2.0"
+import type Gio from "gi://Gio?version=2.0"
 
 import { GObjectify } from "../utils/gobjectify.js"
 import { EntryListModel } from "../utils/entry_list_model.js"
 import { SharedVars } from "../utils/shared_vars.js"
-import { make_iterable } from "../utils/list_model_utils.js"
-import { chunked_idler } from "../utils/async.js"
 import { Entry } from "../utils/entry.js"
 import "../gtk/search_group.js"
 import "../gtk/entry_list.js"
-import { EntryRow } from "../gtk/entry_row.js"
 
 @GObjectify.Class({ template: "/io/github/flattool/Ignition/pages/app_list_page" })
 export class AppListPage extends Adw.NavigationPage {
@@ -21,33 +18,42 @@ export class AppListPage extends Adw.NavigationPage {
 	protected accessor top_model!: Gio.ListModel<Entry>
 
 	@GObjectify.Child
-	protected accessor lb!: Gtk.ListBox
+	protected accessor hidden_filter!: Gtk.BoolFilter
+
+	@GObjectify.Property("bool", { effect(value) { this.update_hidden_filter(value).catch(log) } })
+	public accessor show_hidden_entries!: boolean
+
+	@GObjectify.Property("string")
+	public accessor search_text!: string
 
 	public constructor(params: Adw.NavigationPage.ConstructorProps) {
 		super(params)
 		for (const file of SharedVars.host_app_entry_dirs) {
-			print(file)
 			const model = new EntryListModel({ file })
-			// model.file = file
 			this.entry_list_models_model.append(model)
-			model.connect("items-changed", () => this._items_changed())
+			// model.connect("items-changed", () => this._items_changed())
 		}
+	}
+
+	protected async update_hidden_filter(show_hidden: boolean): Promise<void> {
+		this.hidden_filter.changed(show_hidden ? Gtk.FilterChange.LESS_STRICT : Gtk.FilterChange.MORE_STRICT)
+	}
+
+	protected _on_search_changed(search_entry: Gtk.SearchEntry): void {
+		this.search_text = search_entry.text
+	}
+
+	protected _should_entry_be_shown(__: this, no_display: boolean, entry_enabled: boolean): boolean {
+		return this.show_hidden_entries || (entry_enabled && !no_display)
+	}
+
+	protected _get_no_results(__: this, total_entries: number, total_visible: number): boolean {
+		return total_entries > 0 && total_visible === 0
 	}
 
 	protected _test(): void {
 		for (const model of this.entry_list_models_model) {
 			print(model.monitor, model.file?.get_path())
-		}
-	}
-
-	@GObjectify.Debounce(200)
-	protected async _items_changed(): Promise<void> {
-		this.lb.remove_all()
-		const idler = chunked_idler(100)
-		for (const entry of make_iterable(this.top_model)) {
-			await idler()
-			if (entry.no_display || !entry.enabled) continue
-			this.lb.append(new EntryRow({ entry }))
 		}
 	}
 }
