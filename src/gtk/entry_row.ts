8584badf1a768532ffdf924dde0e198a4b228617
Heliguy <heliguy4599@hotmail.com>
2025-09-12 21:26:00 -0400

    Set EntryRow's suffix_label's CSS classes according to various Entry statuses

--- src/gtk/entry_row.ts
+++ src/gtk/entry_row.ts
@@ -5,6 +5,7 @@ import GLib from "gi://GLib?version=2.0"
 import { GObjectify } from "../utils/gobjectify.js"
 import { Entry } from "../utils/entry.js"
 import { set_icon } from "../utils/icon_helpers.js"
+import { css_switcher } from "../utils/css_switcher.js"
 
 export namespace EntryRow {
 	export interface ConstructorProps {
@@ -18,16 +19,25 @@ export class EntryRow extends Adw.ActionRow {
 	@GObjectify.Child
 	public accessor prefix_icon!: Gtk.Image
 
+	@GObjectify.Child
+	public accessor suffix_label!: Gtk.Label
+
 	@GObjectify.Property(Entry, { flags: "CONSTRUCT_ONLY" })
 	public accessor entry!: Entry
 
 	@GObjectify.Property("bool", { default: true })
 	public accessor show_suffix_label!: boolean
 
+	private label_css_switch = css_switcher(this.suffix_label, "warning")
+
 	public constructor(params: EntryRow.ConstructorProps) {
 		// @ts-expect-error: params has nothing in common with Adw.ActionRow.ConstructorProps,
 		//   but we need to pass our own properties to super
 		super(params)
+	}
+
+	protected update_suffix_label_css(): void {
+		this.label_css_switch(this.entry.override_state === "overridden" || !this.entry.enabled ? "warning" : "")
 	}
 
 	protected _escape_text(__: this, text: string): string {
@@ -47,15 +57,18 @@ export class EntryRow extends Adw.ActionRow {
 		return this.entry.override_state !== "none"
 	}
 
-	protected _get_more_menu_text(entry_override_state: Entry.OveriddenState): string {
-		switch (entry_override_state) {
-			case "overridden": return _("This entry is overridden by a user entry")
-			case "overrides": return _("This entry overrides a system entry.")
-		}
+	protected _get_more_menu_text(__: this, entry_override_state: Entry.OveriddenState): string {
+		if (entry_override_state === "overridden") return _("This entry is overridden by a user entry")
+		if (entry_override_state === "overrides") return _("This entry overrides a system entry.")
 		return ""
 	}
 }
 
 function ready(this: EntryRow): void {
 	set_icon(this.prefix_icon, this.entry.icon)
+	this.update_suffix_label_css()
+
+	this.entry.connect("notify::icon", () => set_icon(this.prefix_icon, this.entry.icon))
+	this.entry.connect("notify::enabled", () => this.update_suffix_label_css())
+	this.entry.connect("notify::override_state", () => this.update_suffix_label_css())
 }
