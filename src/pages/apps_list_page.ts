448c5b0b31d77795ba1f6a36898b1c9a76e28033
heliguy4599 <heliguy4599@hotmail.com>
2026-05-24 15:59:33 -0400

    Fix many errors being printed from closures

--- src/pages/apps_list_page.ts
+++ src/pages/apps_list_page.ts
@@ -1,12 +1,10 @@
 import Adw from "gi://Adw?version=1"
 import Gtk from "gi://Gtk?version=4.0"
 import Gio from "gi://Gio?version=2.0"
-import GObject from "gi://GObject?version=2.0"
 
-import { GClass, Property, Child, Signal, from, Debounce } from "../gobjectify/gobjectify.js"
+import { GClass, Property, Child, Signal, from, Debounce, next_idle } from "../gobjectify/gobjectify.js"
 import { SharedVars } from "../utils/shared_vars.js"
 import { AutostartEntry } from "../utils/autostart_entry.js"
-import { FileList } from "../utils/file_list.js"
 import { EntryGroup } from "../widgets/entry_group.js"
 
 import "../widgets/loading_group.js"
@@ -19,44 +17,41 @@ export class AppsListPage extends from(Adw.NavigationPage, {
 	show_hidden: Property.readwrite.bool(),
 	app_clicked: Signal([AutostartEntry]),
 	_entries: Child<Gio.ListModel<AutostartEntry>>(),
-	_entry_models: Child<Gio.ListStore>(),
 	_entry_sorter: Child<Gtk.CustomSorter>(),
+	_entry_store: Child<Gio.ListStore<AutostartEntry>>(),
 	_entries_group: Child<EntryGroup>(),
 }) {
-	readonly #filters = new Array<Gtk.CustomFilter>()
 	readonly #home_monitor = SharedVars.home_autostart_dir.monitor_directory(Gio.FileMonitorFlags.NONE, null)
 
 	constructor(params?: typeof AppsListPage.$params) {
 		super(params)
 		this._entry_sorter.set_sort_func(AutostartEntry.compare.bind(AutostartEntry))
+		this.#setup_app_list().catch(log)
 		this.#home_monitor.$connect("changed", () => this.#on_home_entries_changed())
-		// TODO: FIX THIS SPAMMING ERRORS
-		// SharedVars.host_app_entry_dirs.forEach(this.#setup_app_list.bind(this))
 	}
 
-	#setup_app_list(directory: Gio.File): void {
-		if (!directory.query_exists(null)) return
-		const file_list = new FileList({ directory })
-		const file_to_entry_model = Gtk.MapListModel.new(file_list.with_implements, this.#entry_map_func.bind(this))
-		const only_entries_filter = Gtk.CustomFilter.new((item: GObject.Object) => (
-			item instanceof AutostartEntry
-			&& !SharedVars.home_autostart_dir.get_child(item.file_name).query_exists(null)
-		))
-		this.#filters.push(only_entries_filter)
-		const filter_model = Gtk.FilterListModel.new(file_to_entry_model, only_entries_filter)
-		this._entry_models.append(filter_model)
-	}
-
-	#entry_map_func(item: GObject.Object): AutostartEntry | GObject.Object {
-		if (!(item instanceof Gio.File)) return item
-		const path: string = item.get_path() ?? ""
-		if (AutostartEntry.verify_file(path) === "") return new AutostartEntry({ path })
-		return item
+	async #setup_app_list(): Promise<void> {
+		this._entry_store.remove_all()
+		for (const directory of SharedVars.host_app_entry_dirs) {
+			if (!directory.query_exists(null)) continue
+			for (const info of directory.enumerate_children(
+				Gio.FILE_ATTRIBUTE_STANDARD_NAME,
+				Gio.FileQueryInfoFlags.NONE,
+				null,
+			)) {
+				await next_idle()
+				const child = directory.get_child(info.get_name())
+				const path = child.get_path() ?? ""
+				if (AutostartEntry.verify_file(path) === "") {
+					this._entry_store.append(new AutostartEntry({ path }))
+				}
+			}
+		}
 	}
 
 	@Debounce(200)
-	#on_home_entries_changed(): void {
-		this.#filters.forEach((filter) => filter.changed(Gtk.FilterChange.DIFFERENT))
+	async #on_home_entries_changed(): Promise<void> {
+		await this.#setup_app_list()
 	}
 
 	protected async _on_search_change(entry: Gtk.SearchEntry): Promise<void> {
